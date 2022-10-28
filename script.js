0ee410da49e11119de4835a1dc05e88a158e3b5e
deajeany16 <cydjeany1601@gmail.com>
2022-10-28 15:12:52 +0700

    Website Rental Mobil, Final Project 1

--- script.js
+++ script.js
@@ -1,12 +1,65 @@
 // INDEX PAGE
 let popup = document.querySelector('.popup')
+let formbook = document.querySelector('.booking-form')
+let databook = document.querySelector('.data-booking')
 
+//show & close pricelist
 function openPopUp() {
   popup.classList.add("showPopUp")
 }
 
 function closePopUp() {
   popup.classList.remove("showPopUp")
+}
+
+//show & close form booking
+function bookingForm() {
+ popup.classList.remove("showPopUp")
+ formbook.classList.add("showForm")
+}
+
+function closeForm() {
+  formbook.classList.remove("showForm")
+}
+
+// save form data to local storage
+const namaInput = document.getElementById("nama")
+const telpInput = document.getElementById("telp")
+const alamatInput = document.getElementById("alamat")
+const carsInput = document.getElementById("cars")
+const harisewaInput = document.getElementById("harisewa")
+const tanggalsewaInput = document.getElementById("tanggalsewa")
+
+function bookCars(event) {
+ let nama = namaInput.value.trim()
+ let telp = telpInput.value.trim()
+ let alamat = alamatInput.value.trim()
+ let cars = carsInput.value.trim()
+ let harisewa = harisewaInput.value.trim()
+ let tanggalsewa = tanggalsewaInput.value.trim()
+     
+  let getbooked = JSON.parse(localStorage.getItem("booksArr"))
+
+  const booksArr = {
+    name : nama,
+    phone : telp,
+    address : alamat,
+    car : cars,
+    days : harisewa,
+    dates : tanggalsewa
+  };
+
+  if(getbooked){
+    nama = booksArr.name
+    telp = booksArr.phone
+    alamat = booksArr.address
+    cars= booksArr.car
+    harisewa = booksArr.days
+    tanggalsewa = booksArr.dates
+  };
+
+  localStorage.setItem("booksArr", JSON.stringify(booksArr))
+  let checkSave = booksArr != null ? alert("Data Pemesanan Tersimpan") : alert("Mohon Lengkapi Form Pemesanan") ;
 }
 
 // TODOS PAGE
@@ -22,7 +75,7 @@ function checkInput(event) {
 function newTodos(event) {
   let todos = inputTodo.value
   let getLocalStorage = localStorage.getItem("todo")
-  let additemLocalStorage = getLocalStorage == null ? todosArr = [] : todosArr = JSON.parse(getLocalStorage);
+  let todosArr = getLocalStorage == null ? [] : JSON.parse(getLocalStorage);
   todosArr.push(todos)
   localStorage.setItem("todo", JSON.stringify(todosArr))
   showTodos()
@@ -30,7 +83,7 @@ function newTodos(event) {
 
 function showTodos() {
   let getLocalStorage = localStorage.getItem("todo")
-  let additemLocalStorage = getLocalStorage == null ? todosArr = [] : todosArr = JSON.parse(getLocalStorage);
+  let todosArr = getLocalStorage == null ? [] : JSON.parse(getLocalStorage);
   const pending = document.querySelector('.footer-todo .pending')
   pending.textContent = todosArr.length
   let newLiTag = ""
